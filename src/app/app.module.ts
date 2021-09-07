a475e5eb78d18d2b84e32cbf6511e0d00c33ad19
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-09-07 16:50:35 +0200

    crate new component 'project'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectComponent } from './project/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
