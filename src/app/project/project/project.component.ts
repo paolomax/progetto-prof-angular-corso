b0084782a0ed0f0ef27c7ab78efb4c5d35987c38
giglioDomenicoMirko <mirko.giglio@gmail.com>
2021-09-07 17:28:22 +0200

    populated array projects

--- src/app/project/project/project.component.ts
+++ src/app/project/project/project.component.ts
@@ -9,7 +9,41 @@ import { Project } from '@app/models/Project';
 
 export class ProjectComponent implements OnInit {
 
-  projects: Project[] = [];
+  projects: Project[] = [
+    {
+      id: 1,
+      code: 'NHusYJl',
+      name: 'Progetto Alpha',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2021, 1, 30),
+      end: new Date(2021, 3, 15),
+      priority: 'medium',
+      done: true,
+      tasks: []
+    },
+    {
+      id: 2,
+      code: 'SJieYKL',
+      name: 'Progetto Beta',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 3, 30),
+      end: new Date(2019, 6, 15),
+      priority: 'low',
+      done: true,
+      tasks: []
+    },
+    {
+      id: 3,
+      code: 'POjeGBs',
+      name: 'Progetto Gamma',
+      description: 'Lorem ipsum dolor sit amet.',
+      start: new Date(2019, 8, 15),
+      priority: 'low',
+      done: false,
+      tasks: []
+    }
+
+  ];
 
   constructor() { }
 
