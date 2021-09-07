57c11705c6ed6b8c1f0153d6c90fc47e204e6c2c
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-09-07 17:17:53 +0200

    Remove label, add projects array

--- src/app/project/project/project.component.ts
+++ src/app/project/project/project.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/Project';
 
 @Component({
   selector: 'ngprj-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
+
 export class ProjectComponent implements OnInit {
-  label = "Io sono una label";
+
+  projects: Project[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
