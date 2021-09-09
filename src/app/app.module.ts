b0ebd113d6bd44b82e83b1fcf190dd4f6164d778
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-09-09 17:10:14 +0200

    add three components into which to subdivide the already created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProjectComponent } from './project/project/project.component';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectComponent
+    ProjectComponent,
+    ProjectDetailComponent
   ],
   imports: [
     BrowserModule,
