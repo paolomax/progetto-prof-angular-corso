6544a0f8cfdb54b5087f2686a28102f5cd1ad741
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-09-11 10:06:17 +0200

    add configuration for project-list component

--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Project } from '@app/models/Project';
 
 @Component({
   selector: 'ngprj-project-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent implements OnInit {
+  
+  @Input() projects : Project[] = [];
+  @Output() selected = new EventEmitter<Project>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  select(project: Project) {
+    this.selected.emit(project);
   }
 
 }
