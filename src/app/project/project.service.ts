5e977de9776fb1c22e862514720491b1a72b9776
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-12 16:26:30 +0200

    add retry, handleError, catchError

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,59 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Project } from '@app/models/Project';
 import { LogService } from '@app/shared/log.service';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, retry, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  projects: Project[] = [
-    {
-      id: 1,
-      code: 'NHusYJl',
-      name: 'Progetto Alpha',
-      description: 'Lorem ipsum dolor sit amet.',
-      start: new Date(2021, 1, 30),
-      end: new Date(2021, 3, 15),
-      priority: 'medium',
-      done: true,
-      tasks: []
-    },
-    {
-      id: 2,
-      code: 'SJieYKL',
-      name: 'Progetto Beta',
-      description: 'Lorem ipsum dolor sit amet.',
-      start: new Date(2019, 3, 30),
-      end: new Date(2019, 6, 15),
-      priority: 'low',
-      done: true,
-      tasks: []
-    },
-    {
-      id: 3,
-      code: 'POjeGBs',
-      name: 'Progetto Gamma',
-      description: 'Lorem ipsum dolor sit amet.',
-      start: new Date(2019, 8, 15),
-      priority: 'low',
-      done: false,
-      tasks: []
-    }
-  ];
-
-  private projectSubject = new BehaviorSubject<Project[]>(this.projects);
-  public projects$ = this.projectSubject.asObservable();
+  // projects: Project[] = [
+  // ];
+  // private projectSubject = new BehaviorSubject<Project[]>(this.projects);
+  // public projects$ = this.projectSubject.asObservable();
 
   constructor(private httpClient: HttpClient, private logService: LogService) {
   }
 
   getAll(): Observable<Project[]> {
     return this.httpClient.get<Project[]>('http://localhost:3000/projects')
-      .pipe(tap((data) => this.logService.log(`getAll Eseguito ${data}`)));
+      .pipe(tap((data) => this.logService.log(`getAll Eseguito ${data}`)), retry(3),
+      catchError(this.handleError));
 
     // return this.projectSubject.pipe(tap(() => this.logService.log('getAllEseguito')));
   }
@@ -72,9 +40,21 @@ export class ProjectService {
 
   get(id: number): Observable<Project> {
     return this.httpClient.get<Project>(`http://localhost:3000/projects/${id}`)
-    .pipe(tap((data) => this.logService.log(`get Eseguito ${data}`)));
+    .pipe(tap((data) => this.logService.log(`get Eseguito ${data}`)), retry(3), catchError(this.handleError));
     // return this.projects.find(project => project.id === id) as Project;
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later');
+  };
 }
 
 
