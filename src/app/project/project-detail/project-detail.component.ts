ac22367e069cd130c1ce046dcadb30489a8c1443
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-16 18:51:12 +0200

    get paramMap data from projectService

--- src/app/project/project-detail/project-detail.component.ts
+++ src/app/project/project-detail/project-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Project } from '@app/models/Project';
+import { switchMap } from 'rxjs/operators';
 import { ProjectService } from '../project.service';
 
 @Component({
@@ -20,9 +21,13 @@ export class ProjectDetailComponent implements OnInit {
     // const id = this.activatedRoute.snapshot.paramMap.get('id');
     // this.projectService.get(+id!).subscribe(data => console.log(data));
 
-    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
-      console.log(params.get('id'));
-    })
+    // this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+    //   console.log(params.get('id'));
+    // })
+
+   this.activatedRoute.paramMap.pipe(
+     switchMap((params: ParamMap) => this.projectService.get(+params.get('id')!))
+     ).subscribe(data => console.log(data));
   }
 
 }
