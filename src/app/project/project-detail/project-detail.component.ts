03ac0d11908dc1112f5181615d529c955077c627
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-16 18:23:33 +0200

    project-detail now gets the project from projectService

--- src/app/project/project-detail/project-detail.component.ts
+++ src/app/project/project-detail/project-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Project } from '@app/models/Project';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'ngprj-project-detail',
@@ -11,9 +13,12 @@ export class ProjectDetailComponent implements OnInit {
 
   @Input() project!: Project;
 
-  constructor() { }
+  constructor( private projectService: ProjectService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.projectService.get(+id!).subscribe(data => console.log(data));
   }
 
 }
