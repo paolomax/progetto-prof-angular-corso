fb3e1761b1edd87261d3118afa7f6ec0c3cc2233
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-19 18:39:59 +0200

    use project-card in project-list

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngprj-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() classes!: string | object | string[];
 
   constructor() { }
 
