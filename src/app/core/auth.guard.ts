8462e15b02d22b891c4ba2ed46611d64e2ea46c7
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-20 16:10:00 +0200

    add AuthenticationGuard in core folder

---
+++ src/app/core/auth.guard.ts
@@ -0,0 +1,27 @@
+// import { Injectable } from "@angular/core";
+// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivateChild } from "@angular/router";
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//     providedIn: 'root'
+// })
+// // export class AuthenticationGuard implements CanActivate, CanActivateChild {
+// //     constructor(private router: Router) {}
+
+// //     canActivate(
+// //         next: ActivatedRouteSnapshot,
+// //         state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | Url
+// //         console.log('You are logged in!');
+// //         return true;
+// //         //this.router.navigate(['signit'], { queryParams: { returnUrl: state.url } } );
+// //         //return false
+// //     }
+
+// //     canActivateChild(
+// //         route: ActivatedRouteSnapshot,
+// //         state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | Url
+// //         return this.canActivate(route, state);
+// //     }
+// // }
+
+// // Guards 4: 53 
